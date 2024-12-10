a02c073b7a40b8865496108f9164b74c6fa38e3c
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 00:04:28 +0530

    Better 👍

--- 1. Python Fundamental/TypesofString.py
+++ 1. Python Fundamental/TypesofString.py
@@ -1,13 +1,14 @@
 ''' String Literals '''
 # Types of String
 
-# Single line String
+# Single line String (' or ")
 a="Hello Welcome to KSK Programming"
 print(a)
 
+
 # Multi line String
 
-# 1st Method (' or ")
+# 1st Method (' or " with \)
 a='Hello Welcome to KSK \
 Programming.Python is very easy \
 to learn langauge.'
