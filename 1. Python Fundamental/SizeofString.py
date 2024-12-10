81a5e5ec595df5cbdc2f4371c70ebacdbd9c608c
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 00:23:03 +0530

    👍

---
+++ 1. Python Fundamental/SizeofString.py
@@ -0,0 +1,32 @@
+""" String Literals """
+# Size of String
+
+'''Note: len() function in Python is a built-in function used to determine the length of an object. It returns the number of items in a given object such as a string, list, tuple, dictionary, set, or other collection types.'''
+
+# Single line String
+a='\\'
+print(len(a))
+
+a="\ab"
+print(len(a))
+
+a="raj\'s phone"
+print(len(a))
+
+a="KSK's"
+print(len(a))
+
+
+# Multi line String
+
+# Multi line String created with triple quote(''' or """)
+str='''a
+b
+c'''
+print(len(str))
+
+# Multi line String created with single or double quote(" or ')
+str1="a\
+b\
+c"
+print(len(str1))
