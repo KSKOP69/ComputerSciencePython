06890d49fc5baec521840a8e63124ea8ae612667
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 01:32:30 +0530

    👍

---
+++ 1. Python Fundamental/DynamicTyping.py
@@ -0,0 +1,11 @@
+""" DynamicTyping """
+
+'''
+Dynamic typing is a feature of some programming languages where variables do not have fixed types, and the type of a variable is determined at runtime rather than at compile-time. This allows you to assign different types of values to the same variable during program execution.
+'''
+
+a=5
+print(a)
+
+a="Hello"
+print(a)
