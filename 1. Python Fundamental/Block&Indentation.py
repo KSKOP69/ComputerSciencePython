2bc4bf36b4a8e0ed8e391e354f03f0cc694b98a9
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 01:27:15 +0530

    👍

---
+++ 1. Python Fundamental/Block&Indentation.py
@@ -0,0 +1,14 @@
+""" Block and Indentation """
+
+'''
+Block: A block is a group of statements that are executed together as a single unit. Block of code begins when a statement ends with a colon (:) and is followed by an indented set of statements.
+
+Indentation: Python uses indentation (spaces or tabs) to define the scope of a block. Python relies on consistent indentation to determine the beginning and end of blocks.
+'''
+
+a=5
+b=10
+if a>b:
+    print("A is greater than B")
+else:
+    print("B is greater than A")
---
+++ 1. Python Fundamental/Vars&Assigment.py
@@ -0,0 +1,27 @@
+""" Variable and Assigment """
+
+'''
+Variable: A variable is a named storage location in a computer program where you can store data. Variables allow programmers to manipulate and reuse data in their programs.
+
+Assignment: Assignment is the process of assigning a value to a variable. It uses the assignment operator (=).
+'''
+# a,b,c are variables
+a=5
+b=15
+c=a+b
+print(c)
+
+
+# Multiple Assigment
+
+# Same value to mutiple variables
+a=b=c=5
+print(a)
+print(b)
+print(c)
+
+# Different values to mutiple variables
+a,b,c=5,15,25
+print(a)
+print(b)
+print(c)
