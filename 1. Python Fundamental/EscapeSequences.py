d6d8e3c9df97ce3d8d7709f0c385d1c97c88958c
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-10 23:43:35 +0530

    👍

---
+++ 1. Python Fundamental/EscapeSequences.py
@@ -0,0 +1,34 @@
+''' String Literals '''
+# Escape Sequences
+
+# \\ Blackslash
+a='Hello \\ World'
+print(a)
+
+# \' Single quote
+a='Hello \' World'
+print(a)
+
+# \" Double quote
+a="Hello \" World"
+print(a)
+
+# \a Ascii bell
+a="Hello\a World"
+print(a)
+
+# \b Backspace
+a="Hello\b World"
+print(a)
+
+# \n New Line
+a="Hello\n World"
+print(a)
+
+# \r Carriage Return
+a="Hello\rWorld"
+print(a)
+
+# \t Tab
+a="Hello\tWorld"
+print(a)
