aa34ccbe9562d21d0c421736073e9ec00f1f4483
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 15:06:29 +0530

    👍

---
+++ 3. Operators/Arthmetic.py
@@ -0,0 +1,29 @@
+""" Arthmetic Operators """
+
+# Add (+)
+a=25+5
+print(a)
+
+# Sub (-)
+a=25-5
+print(a)
+
+# Multi (*)
+a=25*5
+print(a)
+
+# Div (/)
+a=25/5
+print(a)
+
+# Floor Div (//)
+a=5//2
+print(a)
+
+# Remainder/Moduls (%)
+a=25%2
+print(a)
+
+# Exponential
+a=2**10
+print(a)
