1c5184c9bee2e8905da045203363704e280666df
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-11 16:01:03 +0530

    👍

---
+++ 3. Operators/Relational.py
@@ -0,0 +1,31 @@
+""" Relational Operators """
+
+# Less than (<)
+a=10
+b=5
+print(a<b)
+
+# Less than or Equal to (<=)
+a=10
+b=5
+print(a<=b)
+
+# Equal to (==)
+a=10
+b=5
+print(a==b)
+
+# Greater than (>)
+a=10
+b=5
+print(a>b)
+
+# Greater than or Equal to (>=)
+a=10
+b=5
+print(a>=b)
+
+# Not Equal to (!=)
+a=10
+b=5
+print(a!=b)
