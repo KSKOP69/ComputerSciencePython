db62b9106c75c030ae5ac912b66c912449073ed8
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-12 20:20:45 +0530

    👍

---
+++ 6. List Manipulation/5. Replicating.py
@@ -0,0 +1,4 @@
+""" List Manipulation """
+
+list1=["1","2","3"]
+print(list1*4)
---
+++ 6. List Manipulation/6. ListSlicing.py
@@ -0,0 +1,8 @@
+""" List Manipulation """
+
+list1=["Red","Green","Blue","Cyan","Magenta","Black","Yellow"]
+print(list1[2:6])
+print(list1[2:7])
+print(list1[7:2])
+print(list1[:5])
+print(list1[-5:])
