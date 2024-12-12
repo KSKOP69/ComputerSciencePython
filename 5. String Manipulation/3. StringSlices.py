891c6b3cbf1585d87700a9373d5f024986502124
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-12-12 16:27:52 +0530

    👍

---
+++ 5. String Manipulation/3. StringSlices.py
@@ -0,0 +1,25 @@
+""" String Manipulation """
+
+A="Python"
+
+print(A[0:6])
+print(A[0:3])
+print(A[2:5])
+print(A[-5:3])
+print(A[-6:-1])
+print(A[-6:6])
+
+
+# Not defined
+print(A[:6])
+print(A[:])
+print(A[2:])
+print(A[-5:])
+print(A[:-3])
+
+
+# Steping
+print(A[0:6:2])
+print(A[0:6:1])
+print(A[0:6:3])
+print(A[7:9])
