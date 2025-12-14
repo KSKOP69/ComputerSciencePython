c832c2dd98c7b6986a5d782fcb0aa487637c0420
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2025-12-15 02:52:29 +0530

    🥲

--- 5. String Manipulation/1. Traversing.py
+++ 5. String Manipulation/1. Traversing.py
@@ -8,7 +8,7 @@ for k in name:
 # Question:- Reverse the String
 string=input("Enter a String: ")
 print(f"The {string} in Reverse Order is: ")
-lenght=len(string)
-print(lenght)
-for a in range(-1,-lenght-1,-1):
+length=len(string)
+print(length)
+for a in range(-1,-length-1,-1):
     print(string[a])
